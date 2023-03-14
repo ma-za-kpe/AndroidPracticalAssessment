93a6ac74999d884f45e9cf4c586cc809f44dd0f7
ma-za-kpe <makpalyy@gmail.com>
2023-03-14 11:34:18 +0300

    add music knob

--- app/src/main/java/com/maku/amalitechmakumazakpeassessment/MainActivity.kt
+++ app/src/main/java/com/maku/amalitechmakumazakpeassessment/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.maku.amalitechmakumazakpeassessment
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
@@ -20,8 +21,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -49,15 +52,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -79,6 +90,9 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.maku.amalitechmakumazakpeassessment.ui.theme.AmalitechMakuMazakpeAssessmentTheme
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.roundToInt
 import kotlin.random.Random
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -156,17 +170,172 @@ class MainActivity : ComponentActivity() {
 //                AnimationsInCompose()
 
                 // TODO 11: uncomment and move this using navigation button for eight video to its own screen
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize(),
+//                    contentAlignment = Alignment.Center
+//                ) {
+//                    CircularProgressBarInCompose(
+//                        0.8f,
+//                        100
+//                    )
+//                }
+
+                // TODO 12: uncomment and move this using navigation button for eight video to its own screen
                 Box(
+                    contentAlignment = Alignment.Center,
                     modifier = Modifier
-                        .fillMaxSize(),
-                    contentAlignment = Alignment.Center
+                        .fillMaxSize()
+                        .background(
+                            Color(0xFF101010)
+                        )
                 ) {
-                    CircularProgressBarInCompose(
-                        0.8f,
-                        100
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .border(
+                                1.dp,
+                                Color.Green,
+                                RoundedCornerShape(
+                                    10.dp
+                                )
+                            )
+                            .padding(30.dp)
+                    ) {
+                        var volume by remember {
+                            mutableStateOf(0f)
+                        }
+                        val barCount = 20
+                        DragKnob(
+                            modifier = Modifier.size(100.dp)
+                        ) {
+                            volume = it
+                        }
+                        Spacer(
+                            modifier = Modifier.width(20.dp)
+                        )
+                        VolumeBar(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(30.dp),
+                            activeBars = (barCount * volume).roundToInt(),
+                            barCount = barCount
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalComposeUiApi::class)
+    @Composable
+    fun DragKnob(
+        modifier: Modifier = Modifier,
+        limitingAngle: Float = 25f,
+        onValueChange: (Float) -> Unit
+    ) {
+        var rotation by remember {
+            mutableStateOf(limitingAngle)
+        }
+        var touchX by remember {
+            mutableStateOf(0f)
+        }
+        var touchY by remember {
+            mutableStateOf(0f)
+        }
+        var centerX by remember {
+            mutableStateOf(0f)
+        }
+        var centerY by remember {
+            mutableStateOf(0f)
+        }
+
+        Image(
+            painter = painterResource(id = R.drawable.music_knob),
+            contentDescription = "Music knob",
+            modifier = modifier
+                .fillMaxSize()
+                .onGloballyPositioned {
+                    val windowBounds = it.boundsInWindow()
+                    centerX = windowBounds.size.width / 2f
+                    centerY = windowBounds.size.height / 2f
+                }
+                .pointerInteropFilter { event ->
+                    touchX = event.x
+                    touchY = event.y
+                    val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
+
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN,
+                        MotionEvent.ACTION_MOVE -> {
+                            if (angle !in -limitingAngle..limitingAngle) {
+                                val fixedAngle = if (angle in -180f..-limitingAngle) {
+                                    360f + angle
+                                } else {
+                                    angle
+                                }
+                                rotation = fixedAngle
+
+                                val percent =
+                                    (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                                onValueChange(percent)
+                                true
+                            } else false
+                        }
+                        else -> false
+                    }
+                }
+                .rotate(rotation)
+        )
+
+    }
+
+    @Composable
+    fun VolumeBar(
+        modifier: Modifier = Modifier,
+        activeBars: Int = 0,
+        barCount: Int = 10
+    ) {
+        BoxWithConstraints(
+            contentAlignment = Alignment.Center,
+            modifier = modifier
+        ) {
+            val barWidth = remember {
+                constraints.maxWidth / (2f * barCount)
+            }
+            Canvas(
+                modifier = modifier
+            ) {
+                for (i in 0 until barCount) {
+                    drawRoundRect(
+                        color = if (i in 0..activeBars) Color.Green else Color.DarkGray,
+                        topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
+                        size = Size(barWidth, constraints.maxHeight.toFloat()),
+                        cornerRadius = CornerRadius(0f)
                     )
                 }
             }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DragKnobInComposePreview() {
+        AmalitechMakuMazakpeAssessmentTheme {
+            DragKnob(
+                onValueChange = {
+
+                }
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun VolumeBarPreview() {
+        AmalitechMakuMazakpeAssessmentTheme {
+            VolumeBar()
         }
     }
 

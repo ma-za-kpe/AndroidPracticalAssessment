842d2c056166959087649f54635fff4156fd4ccb
ma-za-kpe <makpalyy@gmail.com>
2023-03-18 11:56:40 +0300

    feature: add instagrma ui withought nested scrolling

---
+++ app/src/main/java/com/maku/amalitechmakumazakpeassessment/ui/screen/insta/InstagramScreen.kt
@@ -0,0 +1,20 @@
+package com.maku.amalitechmakumazakpeassessment.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.maku.amalitechmakumazakpeassessment.ui.theme.AmalitechMakuMazakpeAssessmentTheme
+
+@Composable
+fun InstagramScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InstagramUiInComposeUiPreview() {
+    AmalitechMakuMazakpeAssessmentTheme {
+        InstagramScreen(
+
+        )
+    }
+}

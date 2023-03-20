ede67288cf820e4e3073cf8615b05e2d7a2fe969
ma-za-kpe <makpalyy@gmail.com>
2023-03-20 09:18:53 +0300

    add jetpack spalsh screen

--- app/src/main/java/com/maku/amalitechmakumazakpeassessment/MainActivity.kt
+++ app/src/main/java/com/maku/amalitechmakumazakpeassessment/MainActivity.kt
@@ -3,11 +3,13 @@ package com.maku.amalitechmakumazakpeassessment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.maku.amalitechmakumazakpeassessment.ui.MainApp
 import com.maku.amalitechmakumazakpeassessment.ui.theme.AmalitechMakuMazakpeAssessmentTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             AmalitechMakuMazakpeAssessmentTheme {
